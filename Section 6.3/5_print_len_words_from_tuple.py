ee3f7bfbb6edc3bf897f8ec3a854548a253ce8dc
SuperPracion <pracion@bk.ru>
2023-01-09 10:13:12 +0300

    Выполнение модуля 6.3

---
+++ Section 6.3/5_print_len_words_from_tuple.py
@@ -0,0 +1,6 @@
+words_tuple = ('quaint', 'leftovers', 'thesis', 'density', 'retired', 'weak', 'tolerate',
+               'sensitivity', 'primary', 'definition', 'determine', 'bring', 'monstrous',
+               'hurl', 'timetable', 'month', 'advocate', 'provoke', 'stress', 'omission')
+
+for word in words_tuple:
+    print(f'Длина слова {word} = {len(word)}')
