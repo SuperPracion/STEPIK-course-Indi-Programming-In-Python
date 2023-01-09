08101da7d67b5e5ff109e9cc387bd2b801b9e1f8
SuperPracion <pracion@bk.ru>
2023-01-09 19:21:07 +0300

    Выполнение модуля 6.5

---
+++ Section 6.5/1_out_len_a_dict.py
@@ -0,0 +1,11 @@
+account = {
+  "id": 5681,
+  "uid": "45f17b56-bcad-4933-8c73-a37aaa6863b9",
+  "account_number": "6733198878",
+  "iban": "GB79PNXF20678598570754",
+  "bank_name": "AAC CAPITAL PARTNERS LIMITED",
+  "routing_number": "007398728",
+  "swift_bic": "AACCGB21"
+}
+
+print(len(account))
