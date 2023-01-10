8b28618403cfc87f488f77596396e426a28d4af1
SuperPracion <pracion@bk.ru>
2023-01-10 20:50:54 +0300

    Выполнение модуля 6.8

---
+++ Section 6.8/1_dict_generation.py
@@ -0,0 +1,27 @@
+user = {
+    "id": 4170,
+    "uid": "5e941db5-9e0f-4f94-9fc5-734110c6be14",
+    "password": "SyUpfo1ljm",
+    "first_name": "Teresa",
+    "last_name": "Wehner",
+    "username": "teresa.wehner",
+    "email": "teresa.wehner@email.com",
+    "gender": "Non-binary",
+    "phone_number": "+674 424.561.2776",
+    "social_insurance_number": "637316241",
+    "date_of_birth": "1993-08-17",
+    "employment": {
+        "title": "Central Hospitality Liaison",
+        "key_skill": "Organisation"
+    },
+    "subscription": {
+        "plan": "Essential",
+        "status": "Idle",
+        "payment_method": "Debit card",
+        "term": "Annual"
+    }
+}
+
+need_info = input().split()
+
+print({param: user[param] for param in need_info})
