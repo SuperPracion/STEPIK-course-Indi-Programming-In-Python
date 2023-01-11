168de47714a277c8540fddd1535f3e8967383c56
SuperPracion <pracion@bk.ru>
2023-01-11 18:13:21 +0300

    Выполнение модуля 6.11

---
+++ Section 6.11/5_printy_unique_words.py
@@ -0,0 +1,10 @@
+words = ['mention', 'soup', 'pneumonia', 'tradition', 'concert', 'tease', 'generation',
+         'winter', 'national', 'jacket', 'winter', 'wrestle', 'proposal', 'error',
+         'pneumonia', 'concert', 'value', 'value', 'disclose', 'glasses', 'tank',
+         'national', 'soup', 'feel', 'few', 'concert', 'wrestle', 'proposal', 'soup',
+         'sail', 'brown', 'service', 'proposal', 'winter', 'jacket', 'mention', 'tradition',
+         'value', 'feel', 'bear', 'few', 'value', 'winter', 'proposal', 'government',
+         'control', 'value', 'few', 'generation', 'service', 'national',
+         'tradition', 'government', 'mention', 'proposal']
+
+print(len(set(filter(lambda word: len(word) > 6, words))))
